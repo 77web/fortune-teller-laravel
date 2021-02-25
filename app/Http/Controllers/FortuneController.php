ef2bb18c2bc2af66acb6172529aa1ae37919ac96
77web <info@77-web.com>
2021-02-25 19:45:49 +0900

    [refactor-2] FetchFortuneを使ってFortuneを取得する

--- app/Http/Controllers/FortuneController.php
+++ app/Http/Controllers/FortuneController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Domain\DecideSign;
 use App\Models\Fortune;
+use App\Service\FetchFortune;
 use Symfony\Component\HttpFoundation\Request;
 
 class FortuneController extends Controller
@@ -14,16 +15,23 @@ class FortuneController extends Controller
     private $decideSign;
 
     /**
+     * @var FetchFortune
+     */
+    private $fetchFortune;
+
+    /**
      * FortuneController constructor.
      * @param DecideSign $decideSign
+     * @param FetchFortune $fetchFortune
      */
     public function __construct(
-        DecideSign $decideSign
+        DecideSign $decideSign,
+        FetchFortune $fetchFortune
     )
     {
         $this->decideSign = $decideSign;
+        $this->fetchFortune = $fetchFortune;
     }
-
 
     public function index()
     {
@@ -37,7 +45,7 @@ class FortuneController extends Controller
         $targetSign = $this->decideSign->decideSign($birthday);
         abort_unless($targetSign, 400);
 
-        $fortune = Fortune::query()->where('target_date', date('Y-m-d'))->where('target_sign', $targetSign)->get()->first();
+        $fortune = $this->fetchFortune->fetchTodaysFortune($targetSign);
         abort_unless($fortune, 400);
 
         return view('fortune.show', [
