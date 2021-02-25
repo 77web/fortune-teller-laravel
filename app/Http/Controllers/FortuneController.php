5a747bdebf736b1db3643ebb24c1e01813baa32a
77web <info@77-web.com>
2021-02-25 19:10:28 +0900

    [refactor-1] 星座決定をFortuneControllerからDomain\DecideSignクラスを使って行うように

--- app/Http/Controllers/FortuneController.php
+++ app/Http/Controllers/FortuneController.php
@@ -2,11 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Domain\DecideSign;
 use App\Models\Fortune;
 use Symfony\Component\HttpFoundation\Request;
 
 class FortuneController extends Controller
 {
+    /**
+     * @var DecideSign
+     */
+    private $decideSign;
+
+    /**
+     * FortuneController constructor.
+     * @param DecideSign $decideSign
+     */
+    public function __construct(
+        DecideSign $decideSign
+    )
+    {
+        $this->decideSign = $decideSign;
+    }
+
+
     public function index()
     {
         return view('fortune.index');
@@ -15,57 +33,8 @@ class FortuneController extends Controller
     public function show(Request $request)
     {
         $birthday = new \DateTimeImmutable($request->query->get('birthday'));
-        $birthdayValue = (int) $birthday->format('nd');
 
-        $signs = [
-            '牡羊座' => [
-                [321, 419],
-            ],
-            '牡牛座' => [
-                [420, 520],
-            ],
-            '双子座' => [
-                [521, 621],
-            ],
-            '蟹座' => [
-                [622, 722],
-            ],
-            '獅子座' => [
-                [723, 822],
-            ],
-            '乙女座' => [
-                [823, 922],
-            ],
-            '天秤座' => [
-                [923, 1023],
-            ],
-            '蠍座' => [
-                [1024, 1122],
-            ],
-            '射手座' => [
-                [1123, 1221],
-            ],
-            '山羊座' => [
-                [1222,1231],
-                [101, 119],
-            ],
-            '水瓶座' => [
-                [120, 218],
-            ],
-            '魚座' => [
-                [219, 320],
-            ],
-        ];
-
-        $targetSign = null;
-        foreach ($signs as $name => $specs) {
-            foreach ($specs as $spec) {
-                if ($birthdayValue >= $spec[0] && $birthdayValue <= $spec[1]) {
-                    $targetSign = $name;
-                    break 2;
-                }
-            }
-        }
+        $targetSign = $this->decideSign->decideSign($birthday);
         abort_unless($targetSign, 400);
 
         $fortune = Fortune::query()->where('target_date', date('Y-m-d'))->where('target_sign', $targetSign)->get()->first();
